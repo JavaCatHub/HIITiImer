8b4a1d33788811759efe55c562698760fd969c07
Hideki Akiyama <JuniorAndroidDeveloper@protonmail.com>
2019-03-22 19:28:46 +0900

    Make Main Activity
    The ad Id uses a test version.

--- app/src/main/java/com/example/android/hiittimer/MainActivity.java
+++ app/src/main/java/com/example/android/hiittimer/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.android.hiittimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                Toast.makeText(this,"login",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
     }
 }
